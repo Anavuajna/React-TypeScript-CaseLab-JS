6cd9081bff144538aeffa2c8ad2c3c2c3fd578f2
Anavuajna <anavuajna@yandex.ru>
2023-11-14 19:02:18 +0300

    first commit

---
+++ src/components/Filter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { VehicleTypeSelect } from './VehicleTypeSelect'
+
+export class Filter extends React.Component {
+    render(): React.ReactNode {
+        return <div/>;
+    }
+}
